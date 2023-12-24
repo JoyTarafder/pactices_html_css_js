6f0baa5985035e6c47628db74fa4f7dfe6d541f0
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-12-24 17:02:49 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "joytarafder3@gmail.com"
+var accountPassword = "123456"
+let accountState;
+
+accountCity = "Dhaka"
+
+// accountId = 2  // not allow
+accountEmail = "joy@gamil.com"
+accountPassword = "1235"
+accountCity = "Tangail"
+
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+beaceuse of issue in bloack scope and functional scope
+*/
