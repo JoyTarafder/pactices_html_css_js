b94d54749916c6b2418776a0a4b6e0e729f76ddf
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-12-24 17:52:53 +0000

    convertion data types

---
+++ 01_basic/03_convertionOparetion.js
@@ -0,0 +1,29 @@
+let score = "44av"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber); // NaN
+
+//"44" => 44
+// "44ad" => NaN
+// true => 1; false => 0;
+//
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+//"" => false
+//"joy" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber);
