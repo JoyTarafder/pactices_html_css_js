1da5484163f95c92b39b7c14945f9f65346d41b9
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-12-24 17:38:31 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(9 + 0)     // we are using node JS, not browser
+
+console.log(3 
+    + 
+    3); // code readability that should be high
+ 
+let name = "Joy"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefine =>  not assassigned to value
+// symbol => uniqe
+
+// object
+
+
+console.log(typeof null); // this is type of object
+console.log(typeof undefined); // undefundefined 
+    
